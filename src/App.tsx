1687a4c5c58b5b40bc86b5ac623c51d7919ebd05
Richard Harrington <harrington.richard@gmail.com>
2023-04-15 21:53:15 -0400

    some basic renaming of things

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn how to read musical notation
         </a>
       </header>
     </div>
