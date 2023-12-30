eaf84c9308333e1f20aafa17290141a6700b59f9
Richard Harrington <harrington.richard@gmail.com>
2023-12-29 22:10:51 -0500

    added visual metronome component

---
+++ src/components/VisualMetronome.tsx
@@ -0,0 +1,21 @@
+function VisualMetronome() {
+    return (
+      <>
+        <p id="visual-metronome">
+        {/* TODO: Don't hard-code these, make BEATS_PER_BAR of them */}
+        <span id="beat-1">1</span>
+        &nbsp;
+        <span id="beat-2">2</span>
+        &nbsp;
+        <span id="beat-3">3</span>
+        &nbsp;
+        <span id="beat-4">4</span>
+        </p>
+      </>
+    );
+  }
+
+export default VisualMetronome;
+
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import VisualMetronome from './components/VisualMetronome.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <VisualMetronome />
   </React.StrictMode>,
 )
 
